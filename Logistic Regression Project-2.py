5026d8ec3bf5e8bc13d93d5584aa466f05da0101
Adit Modi <adit.modi24@gmail.com>
2020-08-02 13:02:29 +0530

    Add files via upload

---
+++ Logistic Regression Project-2.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ___
+# 
+
+# This data set contains the following features:
+# 
+# * 'Daily Time Spent on Site': consumer time on site in minutes
+# * 'Age': cutomer age in years
+# * 'Area Income': Avg. Income of geographical area of consumer
+# * 'Daily Internet Usage': Avg. minutes a day consumer is on the internet
+# * 'Ad Topic Line': Headline of the advertisement
+# * 'City': City of consumer
+# * 'Male': Whether or not consumer was male
+# * 'Country': Country of consumer
+# * 'Timestamp': Time at which consumer clicked on Ad or closed window
+# * 'Clicked on Ad': 0 or 1 indicated clicking on Ad
+# 
+# ## Import Libraries
+# 
+# **Import a few libraries you think you'll need (Or just import them as you go along!)**
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# ## Get the Data
+# **Read in the advertising.csv file and set it to a data frame called ad_data.**
+
+# In[2]:
+
+
+ad_data= pd.read_csv('advertising.csv')
+
+
+# **Check the head of ad_data**
+
+# In[3]:
+
+
+ad_data.head()
+
+
+# ** Use info and describe() on ad_data**
+
+# In[4]:
+
+
+ad_data.info()
+
+
+# In[5]:
+
+
+ad_data.describe()
+
+
+# ## Exploratory Data Analysis
+# 
+# Let's use seaborn to explore the data!
+# 
+# Try recreating the plots shown below!
+# 
+# ** Create a histogram of the Age**
+
+# In[6]:
+
+
+ad_data['Age'].hist(bins=30,color='darkred',alpha=0.7)
+
+
+# **Create a jointplot showing Area Income versus Age.**
+
+# In[9]:
+
+
+sns.jointplot(x='Age',y='Area Income',data=ad_data)
+
+
+# **Create a jointplot showing the kde distributions of Daily Time spent on site vs. Age.**
+
+# In[10]:
+
+
+sns.jointplot(x='Age',y='Daily Time Spent on Site',data=ad_data,color='red',kind='kde');
+
+
+# ** Create a jointplot of 'Daily Time Spent on Site' vs. 'Daily Internet Usage'**
+
+# In[11]:
+
+
+sns.jointplot(x='Daily Time Spent on Site',y='Daily Internet Usage',data=ad_data,color='green');
+
+
+# ** Finally, create a pairplot with the hue defined by the 'Clicked on Ad' column feature.**
+
+# In[13]:
+
+
+sns.pairplot(ad_data,hue='Clicked on Ad',palette='bwr')
+
+
+# # Logistic Regression
+# 
+# Now it's time to do a train test split, and train our model!
+# 
+# You'll have the freedom here to choose columns that you want to train on!
+
+# ** Split the data into training set and testing set using train_test_split**
+
+# In[14]:
+
+
+ad_data.columns
+
+
+# In[16]:
+
+
+x= ad_data[['Daily Time Spent on Site', 'Age', 'Area Income',
+       'Daily Internet Usage','Male']]
+y= ad_data['Clicked on Ad']
+
+
+# In[19]:
+
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+
+
+# ** Train and fit a logistic regression model on the training set.**
+
+# In[20]:
+
+
+from sklearn.linear_model import LogisticRegression
+
+
+# In[21]:
+
+
+logmodel = LogisticRegression()
+logmodel.fit(x_train,y_train)
+
+
+# ## Predictions and Evaluations
+# ** Now predict values for the testing data.**
+
+# In[22]:
+
+
+predictions = logmodel.predict(X_test)
+
+
+# ** Create a classification report for the model.**
+
+# In[23]:
+
+
+from sklearn.metrics import classification_report
+
+
+# In[24]:
+
+
+print(classification_report(y_test,predictions))
+
+
+# ## Great Job!
---
+++ README.md
@@ -0,0 +1,36 @@
+# Logistic-Regression-with-Python-Part-2
+
+
+Analyse advertising data set, indicating whether or not a particular internet user clicked on an Advertisement.predict whether or not they will click on an ad based off the features of that user
+
+
+![image](https://user-images.githubusercontent.com/48589838/77819233-3cf6cc80-70ff-11ea-8da3-b3e41dbe2b79.png)
+
+
+![image](https://user-images.githubusercontent.com/48589838/77819235-3ff1bd00-70ff-11ea-945c-0ca35e451214.png)
+
+
+![image](https://user-images.githubusercontent.com/48589838/77819237-42ecad80-70ff-11ea-96ed-54d708a4e7a3.png)
+
+
+![image](https://user-images.githubusercontent.com/48589838/77819240-45e79e00-70ff-11ea-8946-645ad113a8a4.png)
+
+
+![image](https://user-images.githubusercontent.com/48589838/77819241-497b2500-70ff-11ea-92c1-26a77c4a5a28.png)
+
+
+ precision    recall  f1-score   support
+
+           0       0.87      0.96      0.91       162
+           
+           1       0.96      0.86      0.91       168
+
+
+    accuracy                           0.91       330
+
+    macro avg       0.91      0.91      0.91       330
+
+    weighted avg       0.91      0.91      0.91       330
+
+
+Reference Material:https://www.udemy.com/share/101WaUB0scc1dQR34=/
